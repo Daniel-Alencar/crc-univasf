003af25539b5f83227bc003d32a4fb05e6074206
Daniel-Alencar <danielalencar746@gmail.com>
2025-04-30 11:24:38 -0300

    ajustes para deploy

--- src/app/(layout)/initial/page.tsx
+++ src/app/(layout)/initial/page.tsx
@@ -8,40 +8,8 @@ import ImageCarousel from '@/components/Carousel';
 import Logo from '@/public/assets/logo.png';
 import ExampleImage from '@/public/assets/projects/project1/image1.png';
 
-import CourseImage from '@/public/assets/rapid_access/courses.png';
-import FormationImage from '@/public/assets/rapid_access/formations.png';
-import NewsImage from '@/public/assets/rapid_access/news.png';
-import ContactImage from '@/public/assets/rapid_access/Contact.png';
-import DigitalPointImage from '@/public/assets/rapid_access/DigitalPoint.png';
-import MakerColonyImage from '@/public/assets/rapid_access/MakerColony.png';
 import Footer from '@/components/Footer';
-
-export const rapidAccessMenu = [
-  {
-    name: "Cursos",
-    image: CourseImage,
-  },
-  {
-    name: "Colônia Maker",
-    image: MakerColonyImage,
-  },
-  {
-    name: "Notícias",
-    image: NewsImage,
-  },
-  {
-    name: "Formaturas",
-    image: FormationImage,
-  },
-  {
-    name: "Ponto digital",
-    image: DigitalPointImage,
-  },
-  {
-    name: "Contato",
-    image: ContactImage,
-  },
-];
+import { rapidAccessMenu } from '@/constants/rapidAccessMenu';
 
 export default function MainPage() {
   return (
---
+++ src/constants/rapidAccessMenu.ts
@@ -0,0 +1,33 @@
+import CourseImage from '@/public/assets/rapid_access/courses.png';
+import FormationImage from '@/public/assets/rapid_access/formations.png';
+import NewsImage from '@/public/assets/rapid_access/news.png';
+import ContactImage from '@/public/assets/rapid_access/Contact.png';
+import DigitalPointImage from '@/public/assets/rapid_access/DigitalPoint.png';
+import MakerColonyImage from '@/public/assets/rapid_access/MakerColony.png';
+
+export const rapidAccessMenu = [
+  {
+    name: "Cursos",
+    image: CourseImage,
+  },
+  {
+    name: "Colônia Maker",
+    image: MakerColonyImage,
+  },
+  {
+    name: "Notícias",
+    image: NewsImage,
+  },
+  {
+    name: "Formaturas",
+    image: FormationImage,
+  },
+  {
+    name: "Ponto digital",
+    image: DigitalPointImage,
+  },
+  {
+    name: "Contato",
+    image: ContactImage,
+  },
+];
