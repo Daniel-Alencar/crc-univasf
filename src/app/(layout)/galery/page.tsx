a8f132322801418c12b1d84e64aa83615a7a7222
Alec-MM <standardsilvercells@gmail.com>
2025-06-12 20:56:15 -0300

    galeria completa

--- src/app/(layout)/galery/page.tsx
+++ src/app/(layout)/galery/page.tsx
@@ -8,11 +8,13 @@ import Image4Project1 from "@/public/assets/projects/project3/4.jpeg";
 import Image5Project1 from "@/public/assets/projects/project3/5.jpeg";
 
 // Projeto 2
-import Image1Project2 from "@/public/assets/projects/project4/6.jpeg";
-import Image2Project2 from "@/public/assets/projects/project4/7.jpeg";
-import Image3Project2 from "@/public/assets/projects/project4/8.jpeg";
-import Image4Project2 from "@/public/assets/projects/project4/9.jpeg";
-import Image5Project2 from "@/public/assets/projects/project4/10.jpeg";
+import Image1Project2 from "@/public/assets/projects/project4/maker1.jpg";
+import Image2Project2 from "@/public/assets/projects/project4/maker2.jpg";
+import Image3Project2 from "@/public/assets/projects/project4/maker3.jpg";
+import Image4Project2 from "@/public/assets/projects/project4/maker4.jpg";
+import Image5Project2 from "@/public/assets/projects/project4/maker5.jpg";
+import Image6Project2 from "@/public/assets/projects/project4/maker6.jpg";
+import Image7Project2 from "@/public/assets/projects/project4/maker7.jpg";
 
 // Projeto 3
 import Image1Project3 from "@/public/assets/projects/project5/11.jpeg";
@@ -58,9 +60,9 @@ import Image20Project5 from "@/public/assets/projects/project7/45.jpeg";
 const projectsData = [
   {
     id: "project-1",
-    title: "Projeto 1",
+    title: "Entrega de Computadores para a Acenibra",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et commodo diam, eget ultrices lorem. Fusce ultrices justo arcu, vitae dapibus urna tincidunt vel. Nullam porta laoreet ex, sit amet scelerisque metus pretium sit amet. Quisque vitae enim accumsan, ultricies risus et, euismod ligula.",
+      " Doação de 10 computadores a ACENIBRA - Associação Nipobrasileira de Juazeiro/BA. Recepcionados pelo vice presidente da associação .",
     images: [
       { src: Image1Project1.src, alt: "Colônia maker 1" },
       { src: Image2Project1.src, alt: "Colônia maker 2" },
@@ -71,22 +73,24 @@ const projectsData = [
   },
   {
     id: "projeto-2",
-    title: "Projeto 2",
+    title: "Colônia Maker",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et commodo diam, eget ultrices lorem. Fusce ultrices justo arcu, vitae dapibus urna tincidunt vel. Nullam porta laoreet ex, sit amet scelerisque metus pretium sit amet. Quisque vitae enim accumsan, ultricies risus et, euismod ligula.",
+      "Aulas de cursos diversos da colônia maker que ocorreu em fevereiro de 2025.",
     images: [
       { src: Image1Project2.src, alt: "Outro projeto 1" },
       { src: Image2Project2.src, alt: "Outro projeto 2" },
       { src: Image3Project2.src, alt: "Outro projeto 3" },
       { src: Image4Project2.src, alt: "Outro projeto 4" },
       { src: Image5Project2.src, alt: "Outro projeto 5" },
+      { src: Image6Project2.src, alt: "Outro projeto 6" },
+      { src: Image7Project2.src, alt: "Outro projeto 7" },
     ],
   },
   {
     id: "projeto-3",
-    title: "Projeto 3",
+    title: "Primeira reunião da nova equipe CRC de 2025",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et commodo diam, eget ultrices lorem. Fusce ultrices justo arcu, vitae dapibus urna tincidunt vel. Nullam porta laoreet ex, sit amet scelerisque metus pretium sit amet. Quisque vitae enim accumsan, ultricies risus et, euismod ligula.",
+      "A nova equipe do CRC realiza a sua primeira reunião para discutir a respeito dos futuros projeto do CRC.",
     images: [
       { src: Image1Project3.src, alt: "Outro projeto 1" },
       { src: Image2Project3.src, alt: "Outro projeto 2" },
@@ -97,9 +101,9 @@ const projectsData = [
   },
   {
     id: "projeto-4",
-    title: "Projeto 4",
+    title: "Evento de entrega de microchips as escolas estaduais de Petrolina e Juazeiro",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et commodo diam, eget ultrices lorem. Fusce ultrices justo arcu, vitae dapibus urna tincidunt vel. Nullam porta laoreet ex, sit amet scelerisque metus pretium sit amet. Quisque vitae enim accumsan, ultricies risus et, euismod ligula.",
+      "Evento de entrega de microchips as escolas estaduais de Petrolina e Juazeiro (15.000) e 800 computadores pelo CRC Univasf, evento realizado no Cineteatro de Petrolina, Univasf, com a presença da Presidente da ACENIBRA como representante dos PIDs que foram montados a partir das entregas e a secretaria da educação de Juazeiro na época e de Petrolina presentes.",
     images: [
       { src: Image1Project4.src, alt: "Outro projeto 1" },
       { src: Image2Project4.src, alt: "Outro projeto 1" },
@@ -115,9 +119,9 @@ const projectsData = [
   },
   {
     id: "projeto-5",
-    title: "Projeto 5",
+    title: "Reunião gerencial realizada em 2024 no MCom",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et commodo diam, eget ultrices lorem. Fusce ultrices justo arcu, vitae dapibus urna tincidunt vel. Nullam porta laoreet ex, sit amet scelerisque metus pretium sit amet. Quisque vitae enim accumsan, ultricies risus et, euismod ligula.",
+      "Imagens da reunião gerencial realizada em 2024 no MCom que ocorreu em Brasília.",
     images: [
       { src: Image1Project5.src, alt: "Outro projeto 1" },
       { src: Image2Project5.src, alt: "Outro projeto 1" },
