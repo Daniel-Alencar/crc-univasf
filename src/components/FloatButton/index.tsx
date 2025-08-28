b947a2c967e481aa27392541e57fb95a489716a7
Daniel-Alencar <danielalencar746@gmail.com>
2025-08-28 17:42:27 -0300

    página de projetos

--- src/components/FloatButton/index.tsx
+++ src/components/FloatButton/index.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default function FloatButton() {
   return (
     <Link href={"https://crc.univasf.edu.br/courses/"}>
-      <div className="fixed bottom-6 right-6 z-50">
+      <div className="fixed bottom-3 right-3 z-50">
         <button
           type="button"
           className="flex items-center gap-2 bg-[#1E88E5] text-white px-6 py-3 rounded-full shadow-lg transform transition-transform hover:scale-110 hover:bg-[#1565C0] active:scale-95 cursor-pointer"
